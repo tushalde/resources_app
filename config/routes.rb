95a4eee3a9b9530888a6bd0bc5211959e0a2de64
Tushal <tushalndesai@gmail.com>
2016-09-20 23:52:44 -0400

    Removing match for all unknown paths in prod from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
   root 'home#index'
   get ':controller(/:action(/:id))'
-  match '*path' => redirect('/')   unless Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
