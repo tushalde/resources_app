fa377a49763db49b91d2842606ac2d631dd83335
Tushal <tushalndesai@gmail.com>
2016-09-20 23:50:47 -0400

    Replacing match with get in routes to deploy on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  match ':controller(/:action(/:id))', via: :get
+  get ':controller(/:action(/:id))'
   match '*path' => redirect('/')   unless Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
