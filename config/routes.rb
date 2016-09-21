447ad300aa32a5f21332e59e23b914d48cced862
Tushal <tushalndesai@gmail.com>
2016-09-20 23:46:46 -0400

    Basic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
 
+  root 'home#index'
+  match ':controller(/:action(/:id))', via: :get
+  match '*path' => redirect('/')   unless Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
